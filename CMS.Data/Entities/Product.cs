60f04f813141f7784f3fcf0e415bbccfb0f1703a
LevYashinn <phuc512dz@gmail.com>
2026-05-16 11:12:44 +0700

    update thêm phần user, post.

--- CMS.Data/Entities/Product.cs
+++ CMS.Data/Entities/Product.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * Sinh viên: Trần Trọng Phúc
+ * Mã sinh viên: 2123110142
+ * Lớp: CCQ2311D
+ * Ngày tạo: 16/05/2026
+ * Mô tả: Thực hiện quản lý danh mục
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
