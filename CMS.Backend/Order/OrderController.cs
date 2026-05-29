a2f7fcb50a979c532459ee5f5e832ec9414f1d72
LevYashinn <phuc512dz@gmail.com>
2026-05-29 15:30:10 +0700

    update thêm bt buổi 2, order, orderdetail, product, customer.

---
+++ CMS.Backend/Order/OrderController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CMS.Data;
+using System.Linq;
+using Microsoft.EntityFrameworkCore; // 1. BẮT BUỘC THÊM DÒNG NÀY
+
+namespace CMS.Backend.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // Action hiển thị danh sách đơn hàng
+        public IActionResult Index()
+        {
+            // 2. THÊM .Include(o => o.Customer) VÀO TRƯỚC .ToList()
+            var orders = _context.Orders
+                                 .Include(o => o.Customer)
+                                 .ToList();
+
+            return View(orders);
+        }
+    }
+}
---
+++ CMS.Backend/OrderDetail/OrderDetailController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CMS.Data;
+using System.Linq;
+using Microsoft.EntityFrameworkCore; // BẮT BUỘC THÊM DÒNG NÀY ĐỂ DÙNG INCLUDE
+
+namespace CMS.Backend.Controllers
+{
+    public class OrderDetailController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderDetailController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // Action hiển thị danh sách chi tiết đơn hàng
+        public IActionResult Index()
+        {
+            // ĐÃ SỬA: Thêm .Include(od => od.Product) để lấy được Tên Sản Phẩm
+            var orderDetails = _context.OrderDetails
+                                       .Include(od => od.Product)
+                                       .ToList();
+
+            return View(orderDetails);
+        }
+    }
+}
